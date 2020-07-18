fbcaf49885b49ceee06f4db0184576878090899d
U-P-G-R-A-Y-E-D-D <sonnekjan@seznam.cz>
2020-07-18 21:24:56 +0200

    Add files via upload

---
+++ barvicky/barvicky/barvicky/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace barvicky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.red = new System.Windows.Forms.HScrollBar();
+            this.green = new System.Windows.Forms.HScrollBar();
+            this.blue = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.lblGreen = new System.Windows.Forms.Label();
+            this.lblBlue = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // red
+            // 
+            this.red.LargeChange = 1;
+            this.red.Location = new System.Drawing.Point(58, 44);
+            this.red.Maximum = 255;
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(148, 13);
+            this.red.TabIndex = 0;
+            this.red.Scroll += new System.Windows.Forms.ScrollEventHandler(this.red_Scroll);
+            // 
+            // green
+            // 
+            this.green.LargeChange = 1;
+            this.green.Location = new System.Drawing.Point(58, 85);
+            this.green.Maximum = 255;
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(148, 13);
+            this.green.TabIndex = 1;
+            this.green.Scroll += new System.Windows.Forms.ScrollEventHandler(this.green_Scroll);
+            // 
+            // blue
+            // 
+            this.blue.LargeChange = 1;
+            this.blue.Location = new System.Drawing.Point(58, 130);
+            this.blue.Maximum = 255;
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(148, 13);
+            this.blue.TabIndex = 2;
+            this.blue.Scroll += new System.Windows.Forms.ScrollEventHandler(this.blue_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "G:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Location = new System.Drawing.Point(234, 44);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(35, 13);
+            this.lblRed.TabIndex = 6;
+            this.lblRed.Text = "label4";
+            // 
+            // lblGreen
+            // 
+            this.lblGreen.AutoSize = true;
+            this.lblGreen.Location = new System.Drawing.Point(234, 85);
+            this.lblGreen.Name = "lblGreen";
+            this.lblGreen.Size = new System.Drawing.Size(35, 13);
+            this.lblGreen.TabIndex = 7;
+            this.lblGreen.Text = "label4";
+            // 
+            // lblBlue
+            // 
+            this.lblBlue.AutoSize = true;
+            this.lblBlue.Location = new System.Drawing.Point(234, 130);
+            this.lblBlue.Name = "lblBlue";
+            this.lblBlue.Size = new System.Drawing.Size(35, 13);
+            this.lblBlue.TabIndex = 8;
+            this.lblBlue.Text = "label4";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(30, 149);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(239, 100);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblBlue);
+            this.Controls.Add(this.lblGreen);
+            this.Controls.Add(this.lblRed);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.green);
+            this.Controls.Add(this.red);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Barvy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HScrollBar red;
+        private System.Windows.Forms.HScrollBar green;
+        private System.Windows.Forms.HScrollBar blue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRed;
+        private System.Windows.Forms.Label lblGreen;
+        private System.Windows.Forms.Label lblBlue;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ barvicky/barvicky/barvicky/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barvicky
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void michani()
+        {
+            lblRed.Text = red.Value.ToString();
+            lblGreen.Text = green.Value.ToString();
+            lblBlue.Text = blue.Value.ToString();
+            panel1.BackColor = Color.FromArgb(red.Value, green.Value, blue.Value);
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            michani();
+        }
+
+        private void red_Scroll(object sender, ScrollEventArgs e)
+        {
+            michani();
+        }
+
+        private void green_Scroll(object sender, ScrollEventArgs e)
+        {
+            michani();
+        }
+
+        private void blue_Scroll(object sender, ScrollEventArgs e)
+        {
+            michani();
+        }
+    }
+}
